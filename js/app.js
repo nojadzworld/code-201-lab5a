ceef375b5206f7d07c8fc148c1eb82e4b3cd3726
DaJon Ewing <dajone37@gmail.com>
2023-02-10 13:11:12 -0800

    prob one

--- js/app.js
+++ js/app.js
@@ -7,13 +7,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
+// // Write your code here
 function sum(a, b) { //eslint-disable-line
+  let mySum = a+b;
+  let myStr = `The sum of ${a} and ${b} is ${mySum}.`;
 
+  return [mySum,myStr];
 }
 
-// Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+// // Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -25,10 +28,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
-function multiply(a, b) { //eslint-disable-line
+// // Write your code here
+// function multiply(a,b) {
+//   let myMul = a * b;
+//   let myAns = `The product of ${a} and ${b} is ${myMul}.`;
 
-}
+//   return [myMul,myAns];
+
+// }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
